6d93b4b7633e54a3b5d2edfc9e0c5dcbf57cf1d2
Daniil Dybka <danil.dybko@gmail.com>
2024-07-02 13:58:10 +0300

    Работает над сайтом

--- js/footer.js
+++ js/footer.js
@@ -2,7 +2,7 @@ function footerList() {
     const footerList = document.querySelector('footer.footer .footer__top ul')
     document.querySelectorAll('nav ul li a').forEach(element =>
         footerList.insertAdjacentHTML('beforeend',
-            `<li><a href="${element.href}" target="${element.target}">${element.textContent}
+            `<li><a href="${element.href}" onclick="navClick('_${new URL(element.href).hash.slice(1)}')" target="${element.target}">${element.textContent}
             ${element.querySelector('svg')?.outerHTML ?? ''}</a></li>`))
 }
 
